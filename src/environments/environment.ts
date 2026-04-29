eba1945f9996ba98350953245d32ec45ad3741e8
Theboul <brad356alex@gmail.com>
2026-04-29 16:43:59 -0400

    docker push web+1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api/v1',
-  //apiUrl: 'https://backend_smart_mechanic.fournext.me/api/v1',
+  //apiUrl: 'http://localhost:8000/api/v1',
+  apiUrl: 'https://backend_smart_mechanic.fournext.me/api/v1',
   aiReportUrl: 'https://josealbertof2004.app.n8n.cloud/webhook/890d6115-a4f8-45aa-95bf-ccbae7c65728/chat',
   firebase: {
     apiKey: "AIzaSyDGZ392VUzOLDf4j7eQvbuRhAPNx24UPVQ",
